ef5261b1218bf3882e0ee92cc5502de23f13d349
Sudipto Chakraborty <71328863+sudipto09@users.noreply.github.com>
2026-05-07 13:08:07 +0200

    Delete prithvi/300mtl.py

--- prithvi/300mtl.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import snapshot_download
-
-model_id_300 = "ibm-nasa-geospatial/Prithvi-EO-2.0-300M-TL"
-local_dir_300 = "./prithvi_300m_tl"
-
-print("Downloading Prithvi-300M-TL weights...")
-snapshot_download(repo_id=model_id_300, local_dir=local_dir_300)
-print("Download complete.")
